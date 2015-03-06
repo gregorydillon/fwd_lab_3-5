d5d4ea9ffebf7d0f56398205881edbdc363c6494
Gregory Dillon <gregorydillon@gmail.com>
2015-03-05 20:16:53 -0800

    Initial merge class material and Github

---
+++ main.js
@@ -0,0 +1,16 @@
+// alert("be carefull");
+// confirm("Are you Sure");
+// prompt("were you careful");
+
+// var brushedTeeth=confirm("did you brush your teeth today");
+
+
+// if(brushedTeeth){
+//   alert("yah, you've got hygine");
+// }
+
+var name = prompt("What is your name");
+typeof "name";
+
+[[{("Greg")}]]
+
